76d1a496b216a4bf7ea68633783b66168432e49a
Neil <neilrowland56@yahoo.com>
2022-12-28 15:19:53 +0000

    signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const User = require("../models/user");
 const uuid = require("uuid");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
-  // console.log(req.body);
   User.findOne({ email: req.body.email }).exec((err, user) => {
     if (user) {
       return res.status(400).json({
@@ -22,12 +23,41 @@ exports.signup = (req, res) => {
           error: err,
         });
       }
-      res.json({
-        user: success,
-      });
       // res.json({
-      //   message: "Signup success! Please signin.",
+      //   user: success,
       // });
+      res.json({
+        message: "Signup success! Please signin.",
+      });
+    });
+  });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // check if user exist
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup.",
+      });
+    }
+    // authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "Email and password do not match.",
+      });
+    }
+    // generate a token and send to client
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, { expiresIn: "1d" });
+    const { _id, username, name, email, role } = user;
+    return res.json({
+      token,
+      user: { _id, username, name, email, role },
     });
   });
 };
